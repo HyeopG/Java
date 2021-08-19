73f2599e2c2fbdb68a4e4a6bf2df3538845d5eac
HyeopG <wndguqdlsla@naver.com>
2021-08-19 13:22:52 +0900

    자료형!

---
+++ HelloJava/src/org/opentutorials/java/datatype/doubleDemo.java
@@ -0,0 +1,11 @@
+package org.opentutorials.java.datatype;
+
+public class doubleDemo {
+
+	public static void main(String[] args) {
+		double a;
+		a = 1.1;
+		
+		System.out.println(a+1.1);
+	}
+}
---
+++ HelloJava/src/org/opentutorials/java/datatype/stringDemo.java
@@ -0,0 +1,18 @@
+package org.opentutorials.java.datatype;
+
+public class stringDemo {
+
+	public static void main(String[] args) {
+		String first;
+		first = "coding";
+		// String first = "coding";
+		
+		System.out.println(first);
+		
+		String a, b;
+		a = "hello";
+		b = "java!";
+		
+		System.out.println(a+b);
+	}
+}
