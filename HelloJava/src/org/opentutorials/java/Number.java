d3c13ae0c3693f34f372eb224d328f76b67a75b4
HyeopG <wndguqdlsla@naver.com>
2021-08-19 11:46:18 +0900

    숫자와 문자 println방법!

---
+++ HelloJava/src/org/opentutorials/java/Number.java
@@ -0,0 +1,13 @@
+package org.opentutorials.java;
+
+public class Number {
+
+	public static void main(String[] args) {
+		
+		System.out.println(1+2);	// 숫자가 파이썬처럼 바로 출력가능!
+		
+		System.out.println(1.2+2.5);// 실수도 바로 출력가능.
+		
+	}
+
+}
---
+++ HelloJava/src/org/opentutorials/java/String.java
@@ -0,0 +1,15 @@
+package org.opentutorials.java;
+
+public class String {
+
+	public static void main(java.lang.String[] args) {
+		System.out.println("안녕하세요.");	// 문자열 출력가능.
+		
+		System.out.println('고' + "기먹고싶다.");	// 문자 + 문자열도 출력가능.
+		
+		System.out.println("고기" + "먹을래");		// 문자열 + 문자열도 가능!
+		
+		System.out.println("egoing said \"Welcome programming world\""); // 따음표를 출력하는 방법!
+	}
+
+}
