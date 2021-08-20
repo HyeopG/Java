801d547af066f4696faddf0bc56aba40127e206f
HyeopG <wndguqdlsla@naver.com>
2021-08-20 11:23:26 +0900

    깔끔한 extends 공부

--- HelloJava/src/org/opentutorials/java/calculator/calculator.java
+++ HelloJava/src/org/opentutorials/java/calculator/calculator.java
@@ -1,10 +1,10 @@
 package org.opentutorials.java.calculator;
 
-public class calculator {
+class CalculatorDemo{
 	int left, right;
 	
-	public calculator() {}
-	public calculator(int left, int right) {
+	public CalculatorDemo() {}
+	public CalculatorDemo(int left, int right) {
 		this.left = left;
 		this.right = right;
 	}
@@ -16,18 +16,9 @@ public class calculator {
 	public void avg() {
 		System.out.println((this.left + this.right) /2);
 	}
-	
-	public static void main(String[] args) {
-		calculator c1 = new calculator(10,20);		// 인스턴스 생성자
-		c1.sum();
-		c1.avg();
-		
-		substractableCalculator c2 = new substractableCalculator(20, 10);
-		c2.sub();
-	}
 }
 
-class substractableCalculator extends calculator{
+class substractableCalculator extends CalculatorDemo{
 
 	public substractableCalculator(int left, int right) {
 		super(left, right);
@@ -35,5 +26,27 @@ class substractableCalculator extends calculator{
 	}
 	public void sub() {
 		System.out.println(this.left - this.right);
+	}
+	
+	public static void main(String[] args) {
+		substractableCalculator c1 = new substractableCalculator(20,10);
+		System.out.println("extends");
+		c1.sum();
+		c1.avg();
+		c1.sub();
+	}
+}
+
+public class calculator {
+	
+	public static void main(String[] args) {
+		CalculatorDemo c1 = new CalculatorDemo(10,20);		// 인스턴스 생성자
+		c1.sum();
+		c1.avg();
+		
+		substractableCalculator c2 = new substractableCalculator(20, 10);
+		System.out.println("------------------------");
+		c2.sum();
+		c2.sub();
 	}
 }
