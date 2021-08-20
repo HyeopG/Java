c4ce1c53d87d33d97c9d997d79f80858ef08f0da
HyeopG <wndguqdlsla@naver.com>
2021-08-20 13:21:00 +0900

    overloading 공부.

---
+++ HelloJava/src/org/opentutorials/java/overloading/overloading.java
@@ -0,0 +1,16 @@
+package org.opentutorials.java.overloading;
+
+public class overloading {
+
+	void A () {System.out.println("void A()");}
+	void A (int arg1) {System.out.println("void A (int arg1)");}
+	void A (String arg1) {System.out.println("void A (String arg1)");}
+	
+	public static void main(String[] args) {
+		overloading od = new overloading();
+		od.A();
+		od.A(1);
+		od.A("coding everybody!");
+	}
+
+}
